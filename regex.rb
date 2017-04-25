f270c571ad73de31debe629216720de9f65a4d19
Mark <markjconlon@gmail.com>
2017-04-25 09:45:35 -0400

    3 number of characters

--- regex.rb
+++ regex.rb
@@ -2,9 +2,19 @@ files = ["mcdonalds.png", "wendys.png", "harveys.jpg"]
 filtered_files = []
 
 files.each do |file|
-  if /png/=~ file
+  if /\.png/=~ file
     filtered_files << file
   end
 end
 
 puts filtered_files
+
+animals = ["beaver", "rat", "dog", "moose"]
+filtered_animals = []
+animals.each do |animal|
+  if /^...$/ =~ animal
+    filtered_animals << animal
+  end
+end
+
+puts filtered_animals
